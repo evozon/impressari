0c583d66a75e1c822c1569e904b7220a12755fb1
unknown <Perl@EVO041L.(none)>
2012-06-09 13:30:17 +0300

    db login

--- lib/Impressari/Models/Base.pm
+++ lib/Impressari/Models/Base.pm
@@ -5,6 +5,6 @@ use warnings;
 
 use base 'Class::DBI::mysql';
 
-__PACKAGE__->connection('dbi:mysql:database=impressari;host=localhost', 'root', '');
+__PACKAGE__->connection('dbi:mysql:database=impressari;host=192.168.2.108', 'impressari', 'impressari');
 
 1;
